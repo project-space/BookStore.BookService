58d97d1663d7bf1275267c340bb308a025416e96
Elladiel <nastyazubastya1999@mail.ru>
2018-01-18 21:36:49 +0300

    added methods GetBook, GetBooks, GetNovelties, GetPopular, GetWithGenre

--- src/BookStore.BookService.Api/Controllers/BooksController.cs
+++ src/BookStore.BookService.Api/Controllers/BooksController.cs
@@ -1,56 +1,43 @@
-﻿using BookStore.BookService.Design.Models;
+﻿using BookStore.BookService.DataAccess;
+using BookStore.BookService.Design.Abstractions.DataAccess;
+using BookStore.BookService.Design.Models;
 using System.Collections.Generic;
 using System.Web.Http;
 
 namespace BookStore.BookService.Api.Controllers
 {
+    [RoutePrefix("api/books")]
     public class BooksController : ApiController
     {
-        public List<Book> Get()
+        private readonly IBookDao bookDao = new BookDao();
+        [HttpGet, Route("{id}")]
+        public Book GetBook(int id)
         {
-            return new List<Book>
-            {
-                new Book
-                {
-                    Author = "Эрих Мария Ремарк",
-                    Title = "Три товарища",
-                    Description = "Все равно в конце все сдохнут.",
-                    Price = 600.50m,
-                    Id = 1
-                },
-                new Book
-                {
-                    Author = "Мария Семенова",
-                    Title = "Волкодав",
-                    Description = "Шедевр всех времен и народов.",
-                    Price = 600,
-                    Id = 2
-                },
-                new Book
-                {
-                    Author = "Джек Лондон",
-                    Title = "Мартин Иден",
-                    Description = "Он утопится.",
-                    Price = 700,
-                    Id = 3
-                },
-                new Book
-                {
-                    Author = "Чак Паланик",
-                    Title = "Бойцовский клуб",
-                    Description = "вере из май майнд",
-                    Price = 900,
-                    Id = 4
-                },
-                new Book
-                {
-                    Author = "Рэй Брэдбери",
-                    Title = "Лёд и пламя",
-                    Description = "Не про Тома и Джерри, как ни странно.",
-                    Price = 800,
-                    Id = 5
-                }
-            };
+            return bookDao.Get(id);
         }
+
+        [HttpPost, Route("byIds")]
+        public List<Book> GetBooks(List<int> ids)
+        {
+            return bookDao.Get(ids);
+        }
+
+        [HttpGet, Route("novelties")]
+        public List<Book> GetNovelties()
+        {
+            return bookDao.GetNovelties();
+        }
+        [HttpGet, Route("popular")]
+        public List<Book> GetPopular()
+        {
+            return bookDao.GetPopular();
+        }
+
+        [HttpGet, Route("withGenre/{id}")]
+        public List<Book> GetWithGenre(int id)
+        {
+            return bookDao.GetWithGenre(id);
+        }
+
     }
 }
